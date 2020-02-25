5d6754e04b0764e31fc86251c49fd3cf651cdc75
James Mahy <james.mahy@cevo.co.uk>
2020-02-25 12:13:00 +0000

    Up dated instructions and removed old code

--- README.md
+++ README.md
@@ -4,9 +4,11 @@
 `docker run -d -p 4100:3000 -p 4106:3306 --name "sosa-app" --rm -v "E:/Development/sosa-app/app/":/var/www/html:Z -t sosa/chat`
 `docker exec -w /var/www/html -it sosa-app npm install`
 
-# Run
+Note: Make sure to rename `examples/message_database_example.db` to `examples/message_database.db` before running! 
+
+# Run Repeater Bot
 From Windows command line with the docker instance running
-`docker exec -it sosa-chat node /var/www/html/app.js`
+`docker exec -it sosa-chat node /var/www/html/RepeaterBots.js`
 
 # Access instance bash
 `docker exec -it sosa-chat bash`
--- bot/app.js
+++ bot/app.js
@@ -1,24 +1,5 @@
 'use strict';
 
-const __filename = import.meta.url;
-const __dirname = new URL(import.meta.url).pathname;
+console.log('Dummy file please launch something from the `examples` folder');
 
-import io from 'socket.io-client';
-
-import { SoSaConfig } from "./config.js";
-import Bot from "./bot.js";
-
-let bot = new Bot(io, SoSaConfig.chat.server, SoSaConfig.chat.api_key);
-
-let onAuthenticated = (authData) => {
-    bot.joinRoom('sosa','general', (room, userList) => {
-        if(room){
-            console.debug(`Joined room ${room.name}`);
-            bot.sendMessage(room.community_id, room.name, 'hello');
-        }
-    });
-};
-
-bot.connect(onAuthenticated);
-
-
+process.exit();
